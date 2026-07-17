b401b6107891ad334ca9280c1f01b2aa9959067c
adityarahi <adityarahi321@gmail.com>
2026-07-17 13:12:20 +0000

    Update include paths after moving KVStore.h to src/store/

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
-#include "KVStore.h"
+#include "store/KVStore.h"
 
 int main() {
   KVStore obj;
--- tests/store_test.cpp
+++ tests/store_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "KVStore.h"
+#include "store/KVStore.h"
 
 class KVStoreTest : public ::testing::Test {
  protected:
